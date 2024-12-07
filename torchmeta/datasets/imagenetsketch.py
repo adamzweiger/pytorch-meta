0957d3eb8e4c99009d18055a8ee717a9bc6c82ff
Janelle Cai <jcai18@mit.edu>
2024-12-07 00:52:21 -0500

    try again

--- torchmeta/datasets/imagenetsketch.py
+++ torchmeta/datasets/imagenetsketch.py
@@ -1,9 +1,8 @@
 import os
-import pickle
-from PIL import Image
 import h5py
 import json
 import numpy as np
+from PIL import Image
 
 from torchmeta.utils.data import Dataset, ClassDataset, CombinationMetaDataset
 
@@ -20,10 +19,6 @@ class ImagenetSketch(CombinationMetaDataset):
             target_transform=target_transform, dataset_transform=dataset_transform)
 
 class ImagenetSketchClassDataset(ClassDataset):
-    folder = 'imagenetsketch'
-    filename = '{0}_data.hdf5'
-    filename_labels = '{0}_labels.json'
-
     def __init__(self, root, meta_train=False, meta_val=False, meta_test=False,
                  meta_split=None, transform=None, class_augmentations=None,
                  download=False):
@@ -31,51 +26,69 @@ class ImagenetSketchClassDataset(ClassDataset):
             meta_val=meta_val, meta_test=meta_test, meta_split=meta_split,
             class_augmentations=class_augmentations)
         
-        self.root = os.path.join(os.path.expanduser(root), self.folder)
+        self.root = root
         self.transform = transform
 
-        # Try multiple potential file paths
-        potential_splits = [meta_split] if meta_split else ['train', 'val', 'test']
-        
-        for potential_split in potential_splits:
-            self.split_filename = self._find_file(self.filename.format(potential_split))
-            self.split_filename_labels = self._find_file(self.filename_labels.format(potential_split))
-            
-            if self.split_filename and self.split_filename_labels:
-                self.meta_split = potential_split
-                break
-        
+        # Potential filenames
+        potential_filenames = [
+            'imagenet_sketch_resized.hdf5',  # HuggingFace upload filename
+            'train_data.hdf5',  # Original torchmeta filename
+        ]
+
+        # Potential label filenames
+        potential_label_filenames = [
+            'imagenet_sketch_labels.json',  # HuggingFace upload filename
+            'train_labels.json',  # Original torchmeta filename
+        ]
+
+        # Find HDF5 file
+        self.hdf5_path = self._find_file(potential_filenames)
+        if not self.hdf5_path:
+            raise FileNotFoundError("Could not find HDF5 dataset file")
+
+        # Find labels file
+        self.labels_path = self._find_file(potential_label_filenames)
+        if not self.labels_path:
+            raise FileNotFoundError("Could not find labels file")
+
+        self._data_file = None
         self._data = None
         self._labels = None
-        self._data_file = None
 
-        # if not self._check_integrity():
-        #     raise RuntimeError('ImagenetSketch integrity check failed')
+        # Populate labels and prepare dataset
         self._num_classes = len(self.labels)
 
-    def _find_file(self, filename):
+    def _find_file(self, possible_filenames):
         """
         Search for the file in multiple potential locations
         """
         search_paths = [
-            os.path.join(self.root, filename),  # Original location
-            os.path.join(self.root, '..', filename),  # Parent directory
-            os.path.join(os.path.expanduser('~'), 'datasets', 'imagenetsketch', filename),  # Home directory
-            os.path.join(os.getcwd(), filename),  # Current working directory
+            self.root,
+            os.path.join(self.root, 'imagenetsketch'),
+            os.path.join(os.path.expanduser('~'), 'datasets', 'imagenetsketch'),
+            os.getcwd()
         ]
         
         for path in search_paths:
-            if os.path.isfile(path):
-                return path
+            for filename in possible_filenames:
+                full_path = os.path.join(path, filename)
+                if os.path.isfile(full_path):
+                    return full_path
         return None
 
     def __getitem__(self, index):
         class_name = self.labels[index % self.num_classes]
-        data = self.data[class_name]
+        
+        # Find indices for this class
+        class_indices = [i for i, label in enumerate(self.labels) if label == class_name]
+        
+        # Select a random index for this class
+        class_index = class_indices[index % len(class_indices)]
+        
         transform = self.get_transform(index, self.transform)
         target_transform = self.get_target_transform(index)
 
-        return ImagenetSketchDataset(index, data, class_name,
+        return ImagenetSketchDataset(index, self.data, class_name, class_index,
             transform=transform, target_transform=target_transform)
 
     @property
@@ -85,35 +98,24 @@ class ImagenetSketchClassDataset(ClassDataset):
     @property
     def data(self):
         if self._data is None:
-            # Support both HDF5 and numpy/pickle formats
-            if self.split_filename.endswith('.hdf5'):
-                self._data_file = h5py.File(self.split_filename, 'r')
+            # Open HDF5 file
+            self._data_file = h5py.File(self.hdf5_path, 'r')
+            
+            # Access the 'datasets' group
+            if 'datasets' in self._data_file:
                 self._data = self._data_file['datasets']
             else:
-                # Fallback to pickle or numpy loading
-                try:
-                    self._data = np.load(self.split_filename)
-                except Exception:
-                    with open(self.split_filename, 'rb') as f:
-                        self._data = pickle.load(f)
+                # Fallback to root-level dataset
+                self._data = self._data_file
         return self._data
 
     @property
     def labels(self):
         if self._labels is None:
-            # Support both JSON and pickle label formats
-            if self.split_filename_labels.endswith('.json'):
-                with open(self.split_filename_labels, 'r') as f:
-                    self._labels = json.load(f)
-            else:
-                # Fallback to pickle
-                with open(self.split_filename_labels, 'rb') as f:
-                    self._labels = pickle.load(f)
+            # Load labels from JSON
+            with open(self.labels_path, 'r') as f:
+                self._labels = json.load(f)
         return self._labels
-
-    def _check_integrity(self):
-        return (self.split_filename is not None and 
-                self.split_filename_labels is not None)
 
     def close(self):
         if self._data_file is not None:
@@ -122,33 +124,24 @@ class ImagenetSketchClassDataset(ClassDataset):
             self._data = None
 
 class ImagenetSketchDataset(Dataset):
-    def __init__(self, index, data, class_name,
+    def __init__(self, index, data, class_name, class_index,
                  transform=None, target_transform=None):
         super(ImagenetSketchDataset, self).__init__(index, transform=transform,
                                                   target_transform=target_transform)
         self.data = data
         self.class_name = class_name
+        self.class_index = class_index
 
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.data)
 
     def __getitem__(self, index):
-        # Handle different data types (numpy array, list of images)
-        if isinstance(self.data, h5py.Dataset):
-            image = Image.fromarray(self.data[index])
-        elif isinstance(self.data, np.ndarray):
-            image = Image.fromarray(self.data[index])
-        elif isinstance(self.data, list):
-            image = Image.fromarray(np.array(self.data[index]))
-        else:
-            raise ValueError(f"Unsupported data type: {type(self.data)}")
-
+        # Get the image from the HDF5 dataset
+        image = Image.fromarray(self.data[self.class_index])
         target = self.class_name
 
         if self.transform is not None:
             image = self.transform(image)
 
         if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return (image, target)
+            ta
